75a8c409efd580f74d0ef0a153cc17689685ac98
Hervé Boutemy <hboutemy@apache.org>
2026-06-13 12:32:06 +0200

    update deployAtEnd ITs to match latest 4 behaviour
    (cherry picked from commit bed4d62dc21edd31f38618b18b10de1016a45e43)

--- src/it/MDEPLOY-225_deploy-at-end-skip-root/verify.groovy
+++ src/it/MDEPLOY-225_deploy-at-end-skip-root/verify.groovy
@@ -17,6 +17,18 @@
  * under the License.
  */
 
-assert ! new File( basedir, "target/repo" ).exists()
-assert new File( basedir, "module1/target/repo/org/apache/maven/its/mdeploy-225/module1/1.0/module1-1.0.pom" ).exists()
-assert new File( basedir, "module2/target/repo/org/apache/maven/its/mdeploy-225/module2/1.0/module2-1.0.pom" ).exists()
+rootRepo = new File( basedir, "target/repo" )
+module1Repo = new File( basedir, "module1/target/repo" )
+module2Repo = new File( basedir, "module2/target/repo" )
+
+if (mavenVersion.startsWith('4.')) {
+    // Maven 4 deploys in the root target directory, as defined in the root POM (even if not deployed)
+    module1Repo = module2Repo = rootRepo
+} else {
+    // Maven 3 deploys in the each module target directory, nothing in root
+    assert ! rootRepo.exists()
+}
+
+// check that files are deployed both by Maven 3 and 4 = https://github.com/apache/maven-deploy-plugin/issues/533
+assert new File( module1Repo, "org/apache/maven/its/mdeploy-225/module1/1.0/module1-1.0.pom" ).exists()
+assert new File( module2Repo, "org/apache/maven/its/mdeploy-225/module2/1.0/module2-1.0.pom" ).exists()
